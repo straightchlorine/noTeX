917080630774e8ed6f67d0b141c61d256b8fbe42
Piotr Krzysztof <piotrlis555@gmail.com>
2023-03-15 16:45:33 +0100

    feat: added the ID class and tidied up existing one

--- noTeX/notes/note_manager.py
+++ noTeX/notes/note_manager.py
@@ -3,43 +3,72 @@
 import os
 from datetime import datetime
 
-from noTeX.exceptions.exc import NoTeXNoSubjectException
+from noTeX.exceptions.exc import NoSubjectPathException
 from noTeX.notes.subjects import NoTeXSubjects
 from noTeX.utility.utils import NoTeXUtility
 
+"""NoTeXNoteId.
+
+Class provides the identification string for each note.
+One ID object accompanies each note object, providing
+easy access to each of the parameters.
+
+For now it is simply (number_of_directories_in_path) + 1.
+
+TODO:
+1. id string initial idea: <main_root>:<subject>:<type>:<internal_id>
+2. export each id to config and import it from config as well
 """
-    NoTeXNoteManager.
+class NoTeXNoteId:
+    note_main_root = ''
+    note_subject = ''
+    note_type = ''
+    note_template = ''
 
-    Class responsible for creating notes.
+    def __init__(self,
+            note_main_root,
+            note_subject,
+            note_type,
+            note_template):
 
-    TODO: proper management
-    1. needs to create the note (base functionality for now)
-    2. needs to create the id of the note (for future navigation)
+        self.note_main_root = note_main_root
+        self.note_subject = note_subject
+        self.note_type = note_type
+        self.note_template = note_template
+
+"""NoTeXNoteManager.
+
+Class responsible for creating notes.
+
+TODO: proper management
+1. needs to create the note (base functionality for now)
+2. needs to create the id of the note (for future navigation)
 """
 class NoTeXNoteManager:
     @staticmethod
-    def __create_note_id(note_main_root, note_subject, note_type, note_template):
-        # id, as a following string:
-        # <main_root>:<subject>:<type>:<internal_id>
-        pass
-
-    @staticmethod
     def __make_note_path(note_subject, note_type, note_template):
+        """Function builds and creates directory for new note.
+
+        Path consists of:
+            <path_to_subject_directory>/
+                <[id]-[creation_date]-[chosen_template]>
+        Where id is (for now) number of directories in the path + 1.
+        """
         # subject path
         s_path = NoTeXSubjects.getpath(note_subject)
         if s_path is None:
-            raise NoTeXNoSubjectException
+            raise NoSubjectPathException('Subject path missing.')
 
-        # path will be built in path variable
+        # base for the note path (starting with subject path)
         path = os.path.join(s_path, note_type)
         if os.path.exists(path):
             os.mkdir(path)
 
-        # creating descriptive name for the directory
-        # <internal_id>-<date>-<template>
-        # where internal_id is simply incremented number of files currently in directory
+        # creating name for the directory
         internal_id = NoTeXUtility.get_dir_size(path)
-        dir_name = str(internal_id) + '-' + datetime.today().strftime('%a-%d-%b-%Y') + '-' + note_template
+        dir_name = str(internal_id) + '-'                        \
+                + datetime.today().strftime('%a-%d-%b-%Y') + '-' \
+                + note_template
         path = os.path.join(path, dir_name)
 
         os.mkdir(path)
