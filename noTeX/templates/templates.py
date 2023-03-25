c78ea58cec9f4d417d4f276c65967d872a221849
Piotr Krzysztof <piotrlis555@gmail.com>
2023-03-25 18:25:23 +0100

    chore: removing bad docs and setting outline

--- noTeX/templates/templates.py
+++ noTeX/templates/templates.py
@@ -2,11 +2,14 @@ import os
 from pathlib import Path
 
 from noTeX.dir.logger import NoTeXLogger
-"""
-    Class responsible for managing latex templates.
 
-    TODO: add code template (artificial one)
 """
+    1. template manager of some sort
+    2. documentation after finishing
+    3. maybe assign parameters to each template in logger
+        class so you can have predefined settings for each
+"""
+
 class NoTeXTemplates:
     # Potential template paths
     __dir = [str(NoTeXLogger.get_opt('templates'))]
@@ -27,33 +30,15 @@ class NoTeXTemplates:
                     if entry.is_dir() and not entry.name == '.git':
                         self.__templates.append(entry)
 
-    """
-        Provide raw DirEntry objects corresponding to each of the templates.
-
-        :return: DirEntry objects of all loaded templates
-        :rtype: list (DirEntry)
-    """
     def get_template_entries(self):
         return self.__templates
 
-    """
-        Provide paths to each of the loaded templates
-
-        :return: paths of all loaded templates
-        :rtype: list (string)
-    """
     def get_template_paths(self):
         paths = []
         for entry in self.__templates:
             paths.append(entry.path)
         return paths
 
-    """
-        Provide names of loaded templates.
-
-        :return: names of all loaded templates
-        :rtype: list (string)
-    """
     def get_template_names(self):
         names = []
         for entry in self.__templates:
